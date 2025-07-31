12c23b733d3cb9db9cd5300275bc056b0788bc9b
John Annunziato <ja3nyc@gmail.com>
2025-07-31 09:56:32 -0400

    refactor: reorder imports in vite config to improve readability

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
+
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 export default defineConfig({
   server: {
