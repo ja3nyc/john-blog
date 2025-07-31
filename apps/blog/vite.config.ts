021869d90c4486754900441adc6fb024a7c683ba
John Annunziato <ja3nyc@gmail.com>
2025-07-31 12:26:57 -0400

    chore: downgrade tanstack router to v1.128.8 and reorganize vite config

--- apps/blog/vite.config.ts
+++ apps/blog/vite.config.ts
@@ -1,18 +1,20 @@
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    tanstackStart({
+      target: 'bun',
+      customViteReactPlugin: true,
+    }),
+    viteReact()
+  ],
   server: {
     port: 3000,
   },
-  plugins: [
-    tsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tanstackStart({ customViteReactPlugin: true, target: 'bun' }),
-    viteReact(),
-  ],
 })
+
+
